50d84fe3acfdfd6591c4fc988d0a23bc674b0b7e
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-24 16:38:22 -0400

    Exported functionality to a Single Card Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
   { "src": "/img/helmet-1.png" },
@@ -31,12 +32,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <div className='card' key={card.id}>
-            <div>
-              <img src={card.src} className='front' alt='card front' />
-              <img src='/img/cover.png' className='back' alt='card back' />
-            </div>
-          </div>
+          <SingleCard key={card.id} card={card} />
         ))}
       </div>
     </div>
